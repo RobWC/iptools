ac3416e9c3e7736abeb837aa8e61b3dc4a28789f
Rob Cameron <rwcameron@gmail.com>
2014-07-21 21:06:02 -0700

    added eazymask to martian check

--- martiancheck/martiancheck.go
+++ martiancheck/martiancheck.go
@@ -2,6 +2,7 @@ package martiancheck
 
 import (
   "net"
+  "github.com/robwc/easymask/ezmv4"
 )
 
 // IPChecker is an interface to check if an IP address is within a martian address range or not.
@@ -21,23 +22,24 @@ type MartianIPv4Checker struct {
 // NewIPv4MartianChecker returns a configured IPv4 Checker struct.
 // Provides all default martian ranges
 func NewMartianIPv4Checker(populate bool) MartianIPv4Checker {
-  ipch := IPv4Checker{}
+  ipch := MartianChecker{}
+  maskch := ezmv4.NewIPv4Masks()
   //create unused blocks
   ipch.IPUnused = make([]net.IPNet,0)
-  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{0,0,0,0},Mask:ipch.Masks["Mask8Bit"]})          //This Net RFC1122
-  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{10,0,0,0},Mask:ipch.Masks["Mask8Bit"]})         //Private IP RFC1918
-  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{127,0,0,0},Mask:ipch.Masks["Mask8Bit"]})        //Loopback IP RFC1122
-  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{169,254,0,0},Mask:ipch.Masks["Mask16Bit"]})     //Local link address
-  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{127,16,0,0},Mask:ipch.Masks["Mask12Bit"]})      //Private IP RFC1918
-  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{192,0,0,0},Mask:ipch.Masks["Mask24Bit"]})       //IETF Protocol Assignments RFC5736
-  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{192,0,2,0},Mask:ipch.Masks["Mask24Bit"]})       //Test-Net-1 RFC5736
-  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{192,168,0,0},Mask:ipch.Masks["Mask16Bit"]})     //Private IP RFC1918
-  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{192,18,0,0},Mask:ipch.Masks["Mask15Bit"]})      //Network Testing
-  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{198,51,100,0},Mask:ipch.Masks["Mask24Bit"]})    //Test-Net-2 RFC5736
-  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{203,0,113,0},Mask:ipch.Masks["Mask24Bit"]})     //Test-Net-3 RFC5736
-  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{224,0,0,0},Mask:ipch.Masks["Mask4Bit"]})        //Multicast RFC3171
-  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{240,0,0,0},Mask:ipch.Masks["Mask4Bit"]})        //Future Use RFC1122
-  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{255,255,255,255},Mask:ipch.Masks["Mask32Bit"]}) //Broadcast
+  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{0,0,0,0},Mask:maskch.Masks["8"]})          //This Net RFC1122
+  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{10,0,0,0},Mask:maskch.Masks["8"]})         //Private IP RFC1918
+  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{127,0,0,0},Mask:maskch.Masks["8"]})        //Loopback IP RFC1122
+  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{169,254,0,0},Mask:maskch.Masks["16"]})     //Local link address
+  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{127,16,0,0},Mask:maskch.Masks["12"]})      //Private IP RFC1918
+  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{192,0,0,0},Mask:maskch.Masks["24"]})       //IETF Protocol Assignments RFC5736
+  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{192,0,2,0},Mask:maskch.Masks["24"]})       //Test-Net-1 RFC5736
+  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{192,168,0,0},Mask:maskch.Masks["16"]})     //Private IP RFC1918
+  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{192,18,0,0},Mask:maskch.Masks["15"]})      //Network Testing
+  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{198,51,100,0},Mask:maskch.Masks["24"]})    //Test-Net-2 RFC5736
+  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{203,0,113,0},Mask:maskch.Masks["24"]})     //Test-Net-3 RFC5736
+  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{224,0,0,0},Mask:maskch.Masks["4"]})        //Multicast RFC3171
+  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{240,0,0,0},Mask:maskch.Masks["4"]})        //Future Use RFC1122
+  ipch.IPUnused = append(ipch.IPUnused,net.IPNet{IP:[]byte{255,255,255,255},Mask:maskch.Masks["32"]}) //Broadcast
   return ipch
 }
 
@@ -71,11 +73,12 @@ func (ipch *IPv4Checker) RemoveAllMartianNet() {
 
 func main() {
   ipch := NewIPv4MartianChecker(true)
+  maskch := ezmv4.NewIPv4Masks()
   log.Println(len(ipch.IPUnused))
   log.Println(ipch.Contains([]byte{11,0,0,1}))
-  ipch.AddMartianNet(net.IPNet{IP:[]byte{11,0,0,0},Mask:ipch.Masks["Mask24Bit"]})
+  ipch.AddMartianNet(net.IPNet{IP:[]byte{11,0,0,0},Mask:maskch.Masks["24"]})
   log.Println(ipch.Contains([]byte{11,0,0,1}))
-  ipch.RemoveMartianNet(net.IPNet{IP:[]byte{11,0,0,0},Mask:ipch.Masks["Mask24Bit"]})
+  ipch.RemoveMartianNet(net.IPNet{IP:[]byte{11,0,0,0},Mask:maskch.Masks["24"]})
   log.Println(ipch.Contains([]byte{11,0,0,1}))
   ipch.RemoveAllMartianNet()
   log.Println(len(ipch.IPUnused))
