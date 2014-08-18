022233d6e1f28892fa008b68e2e63e6084fc9285
Rob Cameron <rwcameron@gmail.com>
2014-08-17 22:51:13 -0700

    Hostmanager API completed

--- hostmanager/hostmanager.go
+++ hostmanager/hostmanager.go
@@ -1,13 +1,23 @@
 package hostmanager
 
 import (
+	"bytes"
+	"errors"
 	"net"
 	"strings"
 )
 
+//HostManager manages a list of IP address
+// contains several helper functions to easily add and remove IPs, supports IPv4 and IPv6
 type HostManager struct {
 	Hosts    []net.IP
 	position int
+}
+
+//NewHostManager creates an initialized host manager
+// default position set to 0
+func NewHostManager() HostManager {
+	return HostManager{position: 0, Hosts: make([]net.IP, 0)}
 }
 
 //AddHost adds a host to the host manager. Accepts IP or hostname strings.
@@ -34,35 +44,90 @@ func (hm *HostManager) AddHostSep(hosts string, sep string) int {
 
 //RemoveHost removes a host from the most manager. Accepts IP or hostname strings.
 // variadic can use string or []string
-func (hm *HostManager) RemoveHost(hosts ...string) int {
-	return 0
+func (hm *HostManager) RemoveHost(hosts ...string) ([]int, int) {
+	hostsRemoved := 0
+	var hostPositions []int
+	for host := range hosts {
+		pos, err := hm.removeHost(net.ParseIP(hosts[host]))
+		if err != nil {
+			//suppress error
+		} else {
+			hostsRemoved = hostsRemoved + 1
+		}
+		hostPositions = append(hostPositions, pos)
+	}
+	return hostPositions, hostsRemoved
+}
+
+//FindHost returns the position of a host or error if not found
+func (hm *HostManager) FindHost(host net.IP) (int, error) {
+	for checkHost := range hm.Hosts {
+		if bytes.Compare(host, hm.Hosts[checkHost]) == 0 {
+			return checkHost, nil
+		}
+	}
+	return -1, errors.New("no host found")
 }
 
 //AddHostIP adds a host to the host manager. Accepts net.IP or []net.IP
-func (hm *HostManager) AddHostIP(ips ...net.IP) {
-
+func (hm *HostManager) AddHostIP(ips ...net.IP) int {
+	var addedHosts int
+	for item := range ips {
+		hm.Hosts = append(hm.Hosts, ips[item])
+		addedHosts = addedHosts + 1
+	}
+	return addedHosts
 }
 
 //RemoveHostIP removes a host from the most manager. Accepts net.IP or []net.IP
 // variadic can use string or []string
-func (hm *HostManager) RemoveHostIP(ips ...net.IP) {
-
+func (hm *HostManager) RemoveHostIP(ips ...net.IP) ([]int, int) {
+	hostsRemoved := 0
+	var hostPositions []int
+	for host := range ips {
+		pos, err := hm.removeHost(ips[host])
+		if err != nil {
+			//suppress error
+		} else {
+			hostsRemoved = hostsRemoved + 1
+		}
+		hostPositions = append(hostPositions, pos)
+	}
+	return hostPositions, hostsRemoved
 }
 
 //NextHost returns the net.IP of the next host
 func (hm *HostManager) NextHost() (net.IP, int) {
-	return net.IP{}, 0
+	if hm.position == len(hm.Hosts) {
+		hm.position = 0
+	} else {
+		hm.position = hm.position + 1
+	}
+	return hm.Hosts[hm.position], hm.position
 }
 
 //PrevHost returns the previous net.IP of the previous host
 func (hm *HostManager) PrevHost() (net.IP, int) {
-return net.IP{}, 0
-
+	if hm.position == 0 {
+		hm.position = len(hm.Hosts) - 1
+	} else if hm.position >= 1 {
+		hm.position = hm.position - 1
+	}
+	return hm.Hosts[hm.position], hm.position
 }
 
 //CurrentHost returns the current IP in the range. It also returns the current position
 func (hm *HostManager) CurrentHost() (net.IP, int) {
-return net.IP{}, 0
+	return hm.Hosts[hm.position], hm.position
 }
 
-func (hm *HostManager) removeHost() {}
+//removeHost
+// remove host internal function to simplify code
+func (hm *HostManager) removeHost(host net.IP) (int, error) {
+	hostPosition, err := hm.FindHost(host)
+	if err != nil {
+		return -1, err
+	}
+	hm.Hosts = append(hm.Hosts[:hostPosition], hm.Hosts[hostPosition+1:]...)
+	return hostPosition, nil
+}
--- hostmanager/hostmanager_test.go
+++ hostmanager/hostmanager_test.go
@@ -15,6 +15,16 @@ func TestAddHost(t *testing.T) {
 		fmt.Println("Host not correctly added")
 		t.Fail()
 	}
+	hm2 := &HostManager{}
+	hm2.AddHost([]string{"1.2.3.4","2.2.3.4"}...)
+	if hm2.Hosts[0].String() == net.ParseIP("1.2.3.4").String() {
+		fmt.Println("Host1 added successfully")
+	} else if hm2.Hosts[1].String() == net.ParseIP("2.2.3.4").String() {
+		fmt.Println("Host2 added successfully")
+	} else {
+		fmt.Println("Hosts not correctly added")
+		t.Fail()
+	}
 }
 
 func TestAddHostSep(t *testing.T) {
@@ -39,6 +49,153 @@ func TestAddHostSep(t *testing.T) {
 		}
 	} else {
 		fmt.Println("Incorrect hosts added")
+		t.Fail()
+	}
+}
+
+func TestFindHost(t *testing.T){
+	hm := &HostManager{}
+	hm.AddHost("1.2.3.4")
+	foundHost,err := hm.FindHost(net.ParseIP("1.2.3.4"))
+	if foundHost == 0 && err == nil {
+		fmt.Println("Host successfully found")
+	} else {
+		fmt.Println("Unable to find host")
+		t.Fail()
+	}
+}
+
+func TestAddHostIP(t *testing.T) {
+	hm := &HostManager{}
+	hm.AddHostIP(net.ParseIP("1.2.3.4"))
+	if hm.Hosts[0].String() == net.ParseIP("1.2.3.4").String() {
+		fmt.Println("Host added successfully")
+	} else {
+		fmt.Println("Host not correctly added")
+		t.Fail()
+	}
+	hm2 := &HostManager{}
+	hm2.AddHostIP([]net.IP{net.ParseIP("1.2.3.4"),net.ParseIP("2.2.3.4")}...)
+	if hm2.Hosts[0].String() == net.ParseIP("1.2.3.4").String() {
+		fmt.Println("Host1 added successfully")
+	} else if hm2.Hosts[1].String() == net.ParseIP("2.2.3.4").String() {
+		fmt.Println("Host2 added successfully")
+	} else {
+		fmt.Println("Hosts not correctly added")
+		t.Fail()
+	}
+}
+
+func TestRemoveHost(t *testing.T) {
+	hm := &HostManager{}
+	hm.AddHost("1.2.3.4")
+	if hm.Hosts[0].String() == net.ParseIP("1.2.3.4").String() {
+		fmt.Println("Host added successfully")
+		hm.RemoveHost("1.2.3.4")
+		if len(hm.Hosts) == 0 {
+			fmt.Println("Host removed successfully")
+		} else {
+			fmt.Println("Host remove failed")
+			t.Fail()
+		}
+	} else {
+		fmt.Println("Host not correctly added")
+		t.Fail()
+	}
+	hm2 := &HostManager{}
+	hm2.AddHost([]string{"1.2.3.4","2.2.3.4"}...)
+	if hm2.Hosts[0].String() == net.ParseIP("1.2.3.4").String() {
+		fmt.Println("Host1 added successfully")
+	} else if hm2.Hosts[1].String() == net.ParseIP("2.2.3.4").String() {
+		fmt.Println("Host2 added successfully")
+	} else {
+		fmt.Println("Hosts not correctly added")
+		t.Fail()
+	}
+
+	pos, removed := hm2.RemoveHost([]string{"1.2.3.4","2.2.3.4"}...)
+	if removed == 2 {
+		fmt.Println("Hosts removed successfully")
+	} else if pos[0] == 0 && pos[1] == 1 {
+		fmt.Println("Correct host positions removed")
+	} else {
+		fmt.Println("Hosts remove failed")
+		t.Fail()
+	}
+}
+
+func TestRemoveHostIP(t *testing.T) {
+	hm := &HostManager{}
+	hm.AddHostIP(net.ParseIP("1.2.3.4"))
+	if hm.Hosts[0].String() == net.ParseIP("1.2.3.4").String() {
+		fmt.Println("Host added successfully")
+		hm.RemoveHost("1.2.3.4")
+		if len(hm.Hosts) == 0 {
+			fmt.Println("Host removed successfully")
+		} else {
+			fmt.Println("Host remove failed")
+			t.Fail()
+		}
+	} else {
+		fmt.Println("Host not correctly added")
+		t.Fail()
+	}
+	hm2 := &HostManager{}
+	hm2.AddHostIP([]net.IP{net.ParseIP("1.2.3.4"),net.ParseIP("2.2.3.4")}...)
+	if hm2.Hosts[0].String() == net.ParseIP("1.2.3.4").String() {
+		fmt.Println("Host1 added successfully")
+	} else if hm2.Hosts[1].String() == net.ParseIP("2.2.3.4").String() {
+		fmt.Println("Host2 added successfully")
+	} else {
+		fmt.Println("Hosts not correctly added")
+		t.Fail()
+	}
+
+	pos, removed := hm2.RemoveHostIP([]net.IP{net.ParseIP("1.2.3.4"),net.ParseIP("2.2.3.4")}...)
+	if removed == 2 {
+		fmt.Println("Hosts removed successfully")
+	} else if pos[0] == 0 && pos[1] == 1 {
+		fmt.Println("Correct host positions removed")
+	} else {
+		fmt.Println("Hosts remove failed")
+		t.Fail()
+	}
+}
+
+func TestNextHost(t *testing.T) {
+	hm := NewHostManager()
+	hm.AddHost("1.2.3.4")
+	hm.AddHost("2.2.3.4")
+	host,curPos := hm.NextHost()
+	if host.String() == "2.2.3.4" && curPos == 1 {
+		fmt.Println("Next host successfully found")
+	} else {
+		fmt.Println("Unable to find next host")
+		t.Fail()
+	}
+}
+
+func TestPrevHost(t *testing.T) {
+	hm := NewHostManager()
+	hm.AddHost("1.2.3.4")
+	hm.AddHost("2.2.3.4")
+	host,curPos := hm.PrevHost()
+	if host.String() == "2.2.3.4" && curPos == 1 {
+		fmt.Println("Previous host successfully found")
+	} else {
+		fmt.Println("Unable to find previous host")
+		t.Fail()
+	}
+}
+
+func TestCurrentHost(t *testing.T) {
+	hm := NewHostManager()
+	hm.AddHost("1.2.3.4")
+	host,curPos := hm.CurrentHost()
+	if host.String() == "1.2.3.4" && curPos == 0 {
+		fmt.Println("Current host successfully found")
+	} else {
+		fmt.Println("Unable to find current host")
 		t.Fail()
 	}
 }
